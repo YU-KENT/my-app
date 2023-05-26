2eca258c1f58767e9ab065d5ed3dcb0c97157e00
YU KENT <tianyu.tina@gmail.com>
2023-05-26 09:31:31 +0200

    add Piechart

--- src/components/Graphique/PieChart.js
+++ src/components/Graphique/PieChart.js
@@ -1,32 +1,26 @@
 
-
-import { PieChart, Pie} from 'recharts';
-
-
+import { PieChart, Pie,Cell,Sector,Text} from 'recharts';
+import '../../Style/PieChart.css'
+ const COLORS = ["#FF0000","#FBFBFB"]
 
 function GraphiquePieChart({data}) {
-    console.log("score",data.score)
-
+    console.log("score",data)
     return (
-        <><span className='LineChart_title'>Durée moyenne des sessions</span>
-        <PieChart width={800} height={400} >
-            <Pie
-                data={data}
-                cx={120}
-                cy={200}
-                innerRadius={60}
-                outerRadius={80}
-                fill="#8884d8"
-                paddingAngle={5}
-                dataKey="value"
-            ></Pie>
+        <><span className='PieChart_title'>Score</span>
+           <p className='PieChart_title_score'>{data[0].name}</p>
+           <p className='PieChart_title_text'>de votre objectif</p>
+        <PieChart width={258} height={200} >
+            <Pie data={data} innerRadius={80} outerRadius={90} stroke='#FBFBFB'
+                dataKey="value" radius={[3, 3, 0, 0]} startAngle={90}>
+             {data.map((entry,index)=>(
+              <Cell key={`cell-${index}`} fill={COLORS[index]} />
+             ))}
+            </Pie>
+            <Pie data={data} dataKey="value" innerRadius={0}  outerRadius={80}  fill="#FFFF">
+            </Pie>
         </PieChart></>
-        
         )
-
 }
   
-
-    
 
 export default GraphiquePieChart
