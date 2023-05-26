f9fde47210580f4e775a82c95d6f430d90f81e5e
YU KENT <tianyu.tina@gmail.com>
2023-05-26 09:31:06 +0200

    add lineChart

--- src/components/Graphique/LineChart.js
+++ src/components/Graphique/LineChart.js
@@ -1,15 +1,54 @@
+
 import { LineChart, XAxis,Tooltip ,Line} from 'recharts';
 import'../../Style/LineChart.css'
 
+
+const CustomizedAxisTick= (props) => {
+    const { x, y, payload } = props;
+    return (
+      <g transform={`translate(${x*0.89},${y})`}>
+        <text x={13} y={0} dy={25} textAnchor="middle" fill="#FFFFFF" opacity='0.5' fontSize='12px'>
+          {payload.value}
+        </text>
+      </g>
+    );}
+
+const CustomTooltip1 = ({ active, payload}) => {
+      if (active && payload && payload.length) {
+        return (
+          <div className="lineChart-custom-tooltip1">
+            <p className="lineChart-custom-tooltip1-label">{`${payload[0].value}`} min</p>
+          </div>
+        );
+      }
+      return null;
+    };
+    const tooltipFormatter = ({ value, name }) => {
+      if (name === 'foo') return
+      return null
+  }
+   /*  const CustomTooltip2 = ({ active, payload, label }) => {
+      if (active && payload && payload.length) {
+        return (
+          <div className="custom-tooltip2">
+          </div>
+        );
+      }
+      return null;
+    }; */
+
 function GraphiqueLineChart({data}) {
-    
+  
+    console.log("LineChart LineChart LineChart",data)
     return(
         <><span className='LineChart_title'>Durée moyenne des sessions</span>
-          <LineChart width={258} height={100} data={data}
-                margin={{ top: 35, right: 30, left: 20, bottom: 5 }}>
-                <XAxis dataKey="dayName" />
-                <Tooltip/>
-                <Line type="monotone" dataKey="sessionLength" stroke="#8884d8" />
+          <LineChart width={268} height={160} data={data}>
+                <XAxis dataKey="dayName" tick={<CustomizedAxisTick/>} tickLine={false} axisLine={false} />
+                <Tooltip content={<CustomTooltip1 />}  formatter={tooltipFormatter} name='foo' />
+            {/*     <Tooltip content={<CustomTooltip2 />} position={{ y: 0 }} /> */}
+                <Tooltip />
+                <Line type="natural" dataKey="sessionLength" stroke="#ffff" opacity="0.5" dot ={false} 
+                /* activeDot={{r:4}} */  strokeWidth={2}/>
           </LineChart>
         </>
       )
